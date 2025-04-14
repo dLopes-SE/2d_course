6cbdb1dc172d954cf263cd3ce7296efb4616cf9e
Dylan Lopes <dylananlopes@gmail.com>
2025-04-14 08:57:55 +0100

    added following camera

--- Assets/Scripts/Character/Movement.cs
+++ Assets/Scripts/Character/Movement.cs
@@ -5,8 +5,7 @@ public class Movement : MonoBehaviour
 {
     // Constants
     [SerializeField]
-    float walkSpeed = 2f;
-    bool canMoveLeft = true;
+    float walkSpeed = 20f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,22 +17,7 @@ public class Movement : MonoBehaviour
     void Update()
     {
         float direction = Input.GetAxis("Horizontal");
-        if (direction < 0 && !canMoveLeft)
-        {
-            return;
-        }
-        else if (direction > 0)
-        {
-            if (!canMoveLeft)
-                canMoveLeft = true;
-        }
-
         transform.Translate(walkSpeed * direction * Time.deltaTime, 0, 0);
         transform.localScale = new Vector3(direction < 0 ? -1 : 1, 1, 1);
-    }
-
-    void OnCollisionEnter2D(Collision2D other) {
-        if (other.collider.tag == "left_wall")
-            canMoveLeft = false;
     }
 }
